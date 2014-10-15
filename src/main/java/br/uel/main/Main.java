fd3749b75f05bc24c6d052cfb98212b5652de4e5
Pedro Sena Tanaka <pedro.stanaka@gmail.com>
2014-10-15 20:27:29 -0300

    Adicionado LogBack

--- src/main/java/br/uel/main/Main.java
+++ src/main/java/br/uel/main/Main.java
@@ -5,12 +5,12 @@ import br.uel.functions.BinaryStep;
 import br.uel.perceptron.Perceptron;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		ActivationFunction function = new BinaryStep();
-		Perceptron p = new Perceptron(function);
-		p.readInput("learning_set.csv");
-		p.training();
+
+    public static void main(String[] args) {
+        ActivationFunction function = new BinaryStep();
+        Perceptron p = new Perceptron(function);
+        p.readInput("learning_set.csv");
+        p.training();
 //		p.print();
-	}
+    }
 }
